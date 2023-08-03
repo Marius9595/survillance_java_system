0b4861fc21c5639287e9f31f68f91b171fb3778d
Sstark97 <aitor@leanmind.es>
2023-08-03 18:46:30 +0100

    :recycle: Remove unnecessary sensor dependency in SurveillanceController

--- src/main/java/kata/example/SurveillanceController.java
+++ src/main/java/kata/example/SurveillanceController.java
@@ -1,13 +1,11 @@
 package kata.example;
 
 public class SurveillanceController implements Suscriptor{
-    private Sensor sensor;
     private Recorder recorder;
 
     public void startSurveillance(Sensor sensor, Recorder recorder) {
-        this.sensor = sensor;
         this.recorder = recorder;
-        this.sensor.subscribe(this);
+        sensor.subscribe(this);
     }
 
     @Override
