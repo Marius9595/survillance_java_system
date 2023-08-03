286de45960802c6ff911275ceb2c8c2adbe276af
Sstark97 <aitor@leanmind.es>
2023-08-03 19:47:00 +0100

    :recycle: Add setup method to initialize all test dependencies

--- src/test/java/kata/example/SurveillanceControllerShould.java
+++ src/test/java/kata/example/SurveillanceControllerShould.java
@@ -1,21 +1,28 @@
 package kata.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static kata.example.SensorEvent.*;
 
 public class SurveillanceControllerShould {
-    @Test
-    public void stop_recording_when_not_movement_is_detected() {
-        Sensor sensor = new MovementSensor();
-        Recorder recorder = Mockito.mock(Recorder.class);
-        SurveillanceController controller = new SurveillanceController();
+    Sensor sensor;
+    Recorder recorder;
+    SurveillanceController controller;
+
+    @BeforeEach
+    public void setup() {
+        sensor = new MovementSensor();
+        recorder = Mockito.mock(Recorder.class);
+        controller = new SurveillanceController();
         controller.startSurveillance(
                 sensor,
                 recorder
         );
-
+    }
+    @Test
+    public void stop_recording_when_not_movement_is_detected() {
         sensor.notifySuscriptors(MOVEMENT_NOT_DETECTED);
 
         Mockito.verify(recorder, Mockito.times(1)).stop();
@@ -23,14 +30,6 @@ public class SurveillanceControllerShould {
 
     @Test
     public void start_recording_when_movement_is_detected() {
-        Sensor sensor = new MovementSensor();
-        Recorder recorder = Mockito.mock(Recorder.class);
-        SurveillanceController controller = new SurveillanceController();
-        controller.startSurveillance(
-                sensor,
-                recorder
-        );
-
         sensor.notifySuscriptors(MOVEMENT_DETECTED);
 
         Mockito.verify(recorder, Mockito.times(1)).start();
@@ -38,14 +37,6 @@ public class SurveillanceControllerShould {
 
     @Test
     public void stop_recording_when_there_unexpected_error_in_sensor() {
-        Sensor sensor = new MovementSensor();
-        Recorder recorder = Mockito.mock(Recorder.class);
-        SurveillanceController controller = new SurveillanceController();
-        controller.startSurveillance(
-                sensor,
-                recorder
-        );
-
         sensor.notifySuscriptors(ERROR);
 
         Mockito.verify(recorder, Mockito.times(1)).stop();
